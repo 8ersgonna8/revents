703ab5a55787410673c2cbc7b13185d2a8613760
Anthon Johansson <anthon92.johansson@gmail.com>
2019-01-02 20:49:11 +0100

    everything configured

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,11 +1,10 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div>
         <h1>Re-vents</h1>
-          
       </div>
     );
   }
